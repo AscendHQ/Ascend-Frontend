ab7f2fd5936bd9e9ecaa77543e2ded50e7e8df4e
Eniolayo <eniolayodeji@gmail.com>
2023-08-20 11:41:32 +0100

    fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Lottie from "react-lottie-player";
 
 import { Container } from "@/components/layout/dashboard";
 
+import lottieJson from "../../public/coming-soon-animation.json";
+
 export default function Dashboard404() {
   return (
-    <Container headerTitle="Page Not Found">
-      <main className="px-10 py-5 h-full bg-white">
-        <h2>Oops!. Page nor found.</h2>
+    <Container headerTitle="Coming soon...">
+      <main className="px-10 py-5 h-full flex justify-center items-center bg-white">
+        {/* <h2>Oops!. Page nor found.</h2> */}
+        <Lottie
+          loop
+          animationData={lottieJson}
+          play
+          style={{ width: 600, height: 600 }}
+        />
       </main>
     </Container>
   );
