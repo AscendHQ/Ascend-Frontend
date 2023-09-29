f53c6abea42364d5ebc012dffc1aa55cbe3b496e
Eniolayo <eniolayodeji@gmail.com>
2023-09-29 17:55:41 +0100

    chore : proper directory naming

--- src/config/links.tsx
+++ src/config/links.tsx
@@ -16,12 +16,11 @@ export const DASHBOARD_OVERVIEW = "/dashboard";
 // STUDENT
 export const DASHBOARD_STUDENT = "/dashboard/database/students";
 export const STUDENT_ACADEMIC_INFORMATION =
-  "/dashboard/database/students/academic-information";
-export const STUDENT_BIODATA = "/dashboard/database/students/biodata";
-export const STUDENT_BIODATA_UPDATE =
-  "/dashboard/database/students/update-biodata";
-export const STUDENT_ACADEMIC_INFORMATION_UPDATE =
   "/dashboard/database/students/update-academic-information";
+export const STUDENT_BIODATA = "/dashboard/database/students/update-biodata";
+export const NEW_STUDENT_BIODATA = "/dashboard/database/students/biodata";
+export const NEW_STUDENT_ACADEMIC_INFORMATION =
+  "/dashboard/database/students/academic-information";
 
 // SUBJECT
 export const DASHBOARD_SUBJECT = "/dashboard/database/subjects";
