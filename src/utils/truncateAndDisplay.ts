9d75adf2e590ce078caaf7fc3e840fa1f0e18ef4
Eniolayo <eniolayodeji@gmail.com>
2023-11-19 22:45:43 +0100

    fix: truncate list function

--- src/utils/truncateAndDisplay.ts
+++ src/utils/truncateAndDisplay.ts
@@ -10,23 +10,23 @@ type SubjectType = string;
  *
  * `Example`: truncateAndDisplay("Joseph,John,Stephen,Jude",2)
  *
- * `Output`: `Joseph,John, +2 more`
+ * `Output`: `Joseph, John , +2 more`
  */
 export default function truncateAndDisplay(
   subject: SubjectType,
   maxItems: number = 10
 ): string {
+  const subjectArray = subject.split(",");
   // Split the subject string by commas, trim each name, take the first 'maxItems', and join them with commas
-  const trimmedNames: string = subject
-    .split(",")
-    .map((name: string) => name.trim())
+  const trimmedNames: string = subjectArray
     .slice(0, maxItems)
+    .map((name: string) => name.trim())
     .join(", ");
 
   // Determine if there are more items beyond 'maxItems' and construct the "+ more" message if needed
   const moreItems: string =
-    subject.split(",").length > maxItems
-      ? ` +${subject.split(",").length - maxItems} more`
+    subjectArray.length > maxItems
+      ? ` +${subjectArray.length - maxItems} more`
       : "";
 
   // Combine the trimmed names and the "+ more" message (if applicable) and return the result
